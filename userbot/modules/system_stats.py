ec6739ef8afcf96aaa7f5fabc4b7425998c9f35f
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-03-16 14:35:52 +0530

    Update system_stats.py
    imp changes

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -123,20 +123,15 @@ async def pipcheck(pip):
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("`"
-                     "i am alive My Mastor \n\n"
-                     " \n\n"
-                     "botdo is alive \n\n"
+                     "`i am alive My Mastor` \n"
+                     "`i can't die` \n"
                      f"Telethon version: {version.__version__} \n"
                      f"Python: {python_version()} \n"
                      f"------------------------------------ \n"
                      f"Website: https://www.facebook.com/Teknoways \n"
-                     " \n\n"
                      f"User: {DEFAULTUSER} \n"
-                     " \n\n"
                      f"Maintainer: @Mayur_Karaniya \n"
-                     " \n\n"
                      f"Admin: @Three_Cube_TeKnoways \n"
-                     " \n\n"
                      f"Userbot: @testing_userbot "
                      "`")    
 
