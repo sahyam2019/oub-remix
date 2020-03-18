96374379ac3331dff9d96192c9662e9dc24d3625
sahyam chakraborty <57591677+sahyam2019@users.noreply.github.com>
2020-03-19 01:25:13 +0530

    Update chat.py

--- userbot/modules/chat.py
+++ userbot/modules/chat.py
@@ -75,7 +75,7 @@ async def log(log_text):
 @register(outgoing=True, pattern="^.kickme$")
 async def kickme(leave):
     """ Basically it's .kickme command """
-    await leave.edit("Nope, no, no, I go away")
+    await leave.edit("Master left the chat 😛")
     await leave.client.kick_participant(leave.chat_id, 'me')
 
 
