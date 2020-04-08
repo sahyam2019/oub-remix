e096df560112296c8e777dfa2a432fa1d4915ba1
sahyam chakraborty <57591677+sahyam2019@users.noreply.github.com>
2020-04-09 01:35:06 +0530

    Update __init__.py

--- userbot/remix/__init__.py
+++ userbot/remix/__init__.py
@@ -1,18 +1,9 @@
-# TG-UserBot - A modular Telegram UserBot script for Python.
-# Copyright (C) 2019  Kandarp <https://github.com/kandnub>
+# Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# TG-UserBot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
 #
-# TG-UserBot is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with TG-UserBot.  If not, see <https://www.gnu.org/licenses/>.
+""" Userbot initialization. """
 
 import configparser
 import logging
@@ -26,135 +17,221 @@ from telethon.tl import types
 from sessions.redis import RedisSession
 from .utils.config_helper import resolve_env
 from .utils.client import UserBotClient
+import os
 
-__version__ = "0.5"
-__license__ = "GNU General Public License v3.0"
-__author__ = 'Kandarp <https://github.com/kandnub>'
-__copyright__ = (
-    "TG-UserBot  Copyright (C) 2019  Kandarp <https://github.com/kandnub>")
-LEVELS = {
-    'DEBUG': logging.DEBUG,
-    'INFO': logging.INFO,
-    'ERROR': logging.ERROR,
-    'CRITICAL': logging.CRITICAL
+from sys import version_info
+from logging import basicConfig, getLogger, INFO, DEBUG
+from distutils.util import strtobool as sb
+
+from pylast import LastFMNetwork, md5
+from pySmartDL import SmartDL
+from dotenv import load_dotenv
+from requests import get
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+
+load_dotenv("config.env")
+
+# Bot Logs setup:
+CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+
+if CONSOLE_LOGGER_VERBOSE:
+    basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=DEBUG,
+    )
+else:
+    basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                level=INFO)
+LOGS = getLogger(__name__)
+
+if version_info[0] < 3 or version_info[1] < 8:
+    LOGS.info("You MUST have a python version of at least 3.8."
+              "Multiple features depend on this. Bot quitting.")
+    quit(1)
+
+# Check if the config was edited by using the already used variable.
+# Basically, its the 'virginity check' for the config file ;)
+CONFIG_CHECK = os.environ.get(
+    "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
+
+if CONFIG_CHECK:
+    LOGS.info(
+        "Please remove the line mentioned in the first hashtag from the config.env file"
+    )
+    quit(1)
+
+# Telegram App KEY and HASH
+API_KEY = os.environ.get("API_KEY", None)
+API_HASH = os.environ.get("API_HASH", None)
+
+
+# Userbot Session String
+STRING_SESSION = os.environ.get("STRING_SESSION", None)
+
+# Logging channel/group ID configuration.
+BOTLOG_CHATID = int(os.environ.get("BOTLOG_CHATID", None))
+
+# Userbot logging feature switch.
+BOTLOG = sb(os.environ.get("BOTLOG", "False"))
+LOGSPAMMER = sb(os.environ.get("LOGSPAMMER", "False"))
+
+# Bleep Blop, this is a bot ;)
+PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
+
+# Heroku Credentials for updater.
+HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "False"))
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+
+# Github Credentials for updater and Gitupload.
+GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
+GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
+
+# Custom (forked) repo URL for updater.
+UPSTREAM_REPO_URL = os.environ.get(
+    "UPSTREAM_REPO_URL",
+    "https://github.com/mkaraniya/OpenUserBot.git")
+
+# Console verbose logging
+CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+
+# SQL Database URI
+DB_URI = os.environ.get("DATABASE_URL", None)
+
+# OCR API key
+OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
+
+# remove.bg API key
+REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+
+# Chrome Driver and Headless Google Chrome Binaries
+CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
+GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
+
+# OpenWeatherMap API Key
+OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
+WEATHER_DEFCITY = os.environ.get("WEATHER_DEFCITY", None)
+
+# Lydia API
+LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
+
+# set blacklist_chats where you do not want userbot's features
+UB_BLACK_LIST_CHAT = os.environ.get("UB_BLACK_LIST_CHAT", "")
+    
+# Anti Spambot Config
+ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT", "False"))
+ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
+
+# Youtube API key
+YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
+
+# Default .alive name
+ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
+
+# Time & Date - Country and Time Zone
+COUNTRY = str(os.environ.get("COUNTRY", ""))
+TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
+
+# Clean Welcome
+CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
+
+# Last.fm Module
+BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
+DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
+
+LASTFM_API = os.environ.get("LASTFM_API", None)
+LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
+LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
+LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
+LASTFM_PASS = md5(LASTFM_PASSWORD_PLAIN)
+if LASTFM_API and LASTFM_SECRET and LASTFM_USERNAME and LASTFM_PASS:
+    lastfm = LastFMNetwork(api_key=LASTFM_API,
+                           api_secret=LASTFM_SECRET,
+                           username=LASTFM_USERNAME,
+                           password_hash=LASTFM_PASS)
+else:
+    lastfm = None
+
+# Google Drive Module
+G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
+G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
+G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
+GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
+TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY",
+                                         "./downloads")
+
+# Genius lyrics get this value from https://genius.com/developers both has same values
+GENIUS_API_TOKEN = os.environ.get("GENIUS", None)
+# Genius lyrics get this value from https://genius.com/developers both has same values
+GENIUS = os.environ.get("GENIUS_API_TOKEN", None)
+
+
+
+# Setting Up CloudMail.ru and MEGA.nz extractor binaries,
+# and giving them correct perms to work properly.
+if not os.path.exists('bin'):
+    os.mkdir('bin')
+
+binaries = {
+    "https://raw.githubusercontent.com/adekmaulana/megadown/master/megadown":
+    "bin/megadown",
+    "https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py":
+    "bin/cmrudl"
 }
 
-root = pathlib.Path(__file__).parent.parent
+for binary, path in binaries.items():
+    downloader = SmartDL(binary, path, progress_bar=False)
+    downloader.start()
+    os.chmod(path, 0o755)
 
-session = "userbot"
-redis_db = False
-loop = None
-config = configparser.ConfigParser()
-config_file = pathlib.Path(root / 'config.ini')
-sql_session = pathlib.Path(root / 'userbot.session')
+# 'bot' variable
+if STRING_SESSION:
+    # pylint: disable=invalid-name
+    bot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
+else:
+    # pylint: disable=invalid-name
+    bot = TelegramClient("userbot", API_KEY, API_HASH)
 
-ROOT_LOGGER = logging.getLogger()
-LOGGER = logging.getLogger(__name__)
 
-if sys.platform.startswith('win'):
-    from asyncio import ProactorEventLoop
-    from os import system
+async def check_botlog_chatid():
+    if not BOTLOG_CHATID and LOGSPAMMER:
+        LOGS.info(
+            "You must set up the BOTLOG_CHATID variable in the config.env or environment variables, for the private error log storage to work."
+        )
+        quit(1)
 
-    loop = ProactorEventLoop()
-    system('color')
+    elif not BOTLOG_CHATID and BOTLOG:
+        LOGS.info(
+            "You must set up the BOTLOG_CHATID variable in the config.env or environment variables, for the userbot logging feature to work."
+        )
+        quit(1)
 
-if platform.python_version_tuple() < ('3', '7', '3'):
-    print("Please run this script with Python 3.7.3 or above."
-          "\nExiting the script.")
-    sys.exit(1)
+    elif not BOTLOG or not LOGSPAMMER:
+        return
 
-if config_file.exists():
-    config.read(config_file)
-    resolve_env(config)
+    entity = await bot.get_entity(BOTLOG_CHATID)
+    if entity.default_banned_rights.send_messages:
+        LOGS.info(
+            "Your account doesn't have rights to send messages to BOTLOG_CHATID "
+            "group. Check if you typed the Chat ID correctly.")
+        quit(1)
 
-try:
-    resolve_env(config)
-except ValueError:
-    print("Please make sure you have a proper config.ini in this directory "
-          "or the required environment variables set."
-          "\nExiting the script.")
-    sys.exit(1)
 
-if "telethon" not in config:
-    print("You're not using a valid config, refer to the sample_config.ini")
-    sys.exit(1)
-
-telethon = config['telethon']
-API_ID = telethon.getint('api_id', False)
-API_HASH = telethon.get('api_hash', False)
-REDIS_ENDPOINT = telethon.get('redis_endpoint', False)
-REDIS_PASSWORD = telethon.get('redis_password', False)
-
-userbot = config['userbot']
-LOGGER_CHAT_ID = userbot.getint('logger_group_id', 0)
-CONSOLE_LOGGER = userbot.get('console_logger_level', 'INFO')
-
-if CONSOLE_LOGGER.upper() in LEVELS:
-    level = LEVELS[CONSOLE_LOGGER.upper()]
-    ROOT_LOGGER.setLevel(level)
-    LOGGER.setLevel(level)
-
-if not (API_ID and API_HASH):
-    print("You need to set your API keys in your config or environment!")
-    LOGGER.debug("No API keys!")
-    sys.exit(1)
-
-if REDIS_ENDPOINT and REDIS_PASSWORD:
-    REDIS_HOST = REDIS_ENDPOINT.split(':')[0]
-    REDIS_PORT = REDIS_ENDPOINT.split(':')[1]
-    redis_connection = redis.Redis(host=REDIS_HOST,
-                                   port=REDIS_PORT,
-                                   password=REDIS_PASSWORD)
+with bot:
     try:
-        redis_connection.ping()
-    except Exception as e:
-        LOGGER.exception(e)
-        print()
-        LOGGER.error(
-            "Make sure you have the correct Redis endpoint and password "
-            "and your machine can make connections.")
-        sys.exit(1)
-    LOGGER.debug("Connected to Redis successfully!")
-    redis_db = redis_connection
-    if not sql_session.exists():
-        LOGGER.debug("Using Redis session!")
-        session = RedisSession("userbot", redis_connection)
+        bot.loop.run_until_complete(check_botlog_chatid())
+    except:
+        LOGS.info(
+            "BOTLOG_CHATID environment variable isn't a "
+            "valid entity. Check your environment variables/config.env file.")
+        quit(1)
 
-client = UserBotClient(session=session,
-                       api_id=API_ID,
-                       api_hash=API_HASH,
-                       loop=loop,
-                       app_version=__version__,
-                       auto_reconnect=False)
-
-client.version = __version__
-client.config = config
-client.prefix = userbot.get('userbot_prefix', None)
-client.database = redis_db
-
-
-def verifyLoggerGroup(client: UserBotClient) -> None:
-    client.logger = True
-
-    def disable_logger(error: str):
-        if LOGGER_CHAT_ID != 0:
-            LOGGER.error(error)
-        client.logger = False
-
-    try:
-        entity = client.loop.run_until_complete(
-            client.get_entity(LOGGER_CHAT_ID))
-        if not isinstance(entity, types.User):
-            if not entity.creator:
-                if entity.default_banned_rights.send_messages:
-                    disable_logger("Permissions missing to send messages "
-                                   "for the specified Logger group.")
-    except ValueError:
-        disable_logger("Logger group ID cannot be found. "
-                       "Make sure it's correct.")
-    except TypeError:
-        disable_logger("Logger group ID is unsupported. "
-                       "Make sure it's correct.")
-    except Exception as e:
-        disable_logger("An Exception occured upon trying to verify "
-                       "the logger group.\n" + str(e))
+# Global Variables
+COUNT_MSG = 0
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+CMD_HELP = {}
+ISAFK = False
+AFKREASON = None
