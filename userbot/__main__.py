85d3c689149cbb9750d23fea5ac452a838ddae41
sahyam chakraborty <57591677+sahyam2019@users.noreply.github.com>
2020-04-09 21:19:52 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -29,8 +29,8 @@ for module_name in ALL_MODULES:
 LOGS.info("You are running OpenUserBot [v7.7.7]")
 
 LOGS.info(
-    "😘 Congratulations, your userbot is now running !! Test it by typing .ping in any chat 😘."
-    "Once died 😵, but now ON again 😉, A BIG thanks to https://github.com/Devp73 Dev Patel who have helped make this userbot ON AGAIN with his HUGE FIX!!"
+    "😘 WOW your user bot is running  !! Test it by typing .ping in any chat 😘."
+    "A BIG thanks to Dev Patel for helping me with his HUGE FIX!!"
     "for command list type .help & If you need assistance, head to https://t.me/PPE_Support")
 
 if len(argv) not in (1, 3, 4):
