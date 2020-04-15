b189d70fdd8ca2056784d0522123d4b104a5fe30
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-15 23:14:12 +0530

    Update fakegban.py

--- userbot/modules/fakegban.py
+++ userbot/modules/fakegban.py
@@ -4,7 +4,7 @@ import asyncio
 from telethon import events
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import ChannelParticipantsAdmins
-from userbot.utils import admin_cmd
+#from userbot.utils import admin_cmd
 from userbot.events import register 
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot, ALIVE_NAME
 
