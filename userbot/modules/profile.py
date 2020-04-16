b9622230f6fe019b858959bbdeb344f8ca0fb5b6
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-17 02:26:13 +0530

    Update profile.py
    useful af
    never used it though
    version 1.D

--- userbot/modules/profile.py
+++ userbot/modules/profile.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
 """ Userbot module for changing your Telegram profile details. """
@@ -179,7 +179,7 @@ async def remove_profilepic(delpfp):
 
 
 CMD_HELP.update({
-    "profile":
+    "Profilepic":
     ".username <new_username>\
 \nUsage: Changes your Telegram username.\
 \n\n.name <firstname> or .name <firstname> <lastname>\
