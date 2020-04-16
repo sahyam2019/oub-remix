d3cc30ef4d3dfefa58217ba1f95a50b4f96cff61
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-17 01:56:52 +0530

    Update afk.py
    kek

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -4,7 +4,7 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 # All Credits to https://t.me/azrim89 for timestamp.
-#.Added some custom gay things. Ignore it. I hate bots
+#.Added some custom afk strings. Ignore it. I hate bots
 
 """ Userbot module which contains afk-related commands """
 
