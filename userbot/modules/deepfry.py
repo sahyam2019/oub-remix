f66e891b461e7ec3d76df50f9b182b112050ce18
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-17 17:29:08 +0530

    Update deepfry.py

--- userbot/modules/deepfry.py
+++ userbot/modules/deepfry.py
@@ -27,7 +27,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-#Ported Xtra-telegram by @heyworld
+#Ported from Xtra-telegram by @heyworld
 """ Userbot module for frying stuff. ported by @NeoMatrix90 """
 
 import io
