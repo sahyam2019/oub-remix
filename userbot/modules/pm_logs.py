78c46a58685d72af91888fd4f97e9aaee3f3dee3
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-19 15:55:47 +0530

    Create pm_logs.py

---
+++ userbot/modules/pm_logs.py
@@ -0,0 +1,27 @@
+#this module originally created by @spechide https://github.com/SpEcHiDe/UniBorg
+
+import asyncio
+from telethon import events
+from telethon.tl import functions, types
+from userbot.events import register
+from userbot import BOTLOG, BOTLOG_CHATID, LOGS
+
+
+NO_PM_LOG_USERS = []
+
+
+@register(incoming=True, disable_edited=True)
+async def monito_p_m_s(event):
+    sender = await event.get_sender()
+    if event.is_private and not (await event.get_sender()).bot:
+        chat = await event.get_chat()
+        if chat.id not in NO_PM_LOG_USERS and chat.id:
+            try:
+                e = await event.client.get_entity(int(BOTLOG_CHATID))
+                fwd_message = await event.client.forward_messages(
+                    e,
+                    event.message,
+                    silent=True
+                )
+            except Exception as e:
+                LOGS.warn(str(e))
