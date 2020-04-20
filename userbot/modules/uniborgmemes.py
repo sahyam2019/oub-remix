999b0020cd23e9905e1f006b32db6b99c7443273
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-20 20:48:28 +0530

    Update uniborgmemes.py

--- userbot/modules/uniborgmemes.py
+++ userbot/modules/uniborgmemes.py
@@ -475,5 +475,22 @@ async def _(event):
 
             await event.edit(animation_chars[i % 24])
 		
-		
+@register(outgoing=True, pattern="^.gangstar(?: |$)(.*)")
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("EVERyBOdy")
+        await asyncio.sleep(0.3)
+        await event.edit("wAs")
+        await asyncio.sleep(0.2)
+        await event.edit("GanGeSTar")
+        await asyncio.sleep(0.5)
+        await event.edit("UNtIL ")
+        await asyncio.sleep(0.2)
+        await event.edit("I")
+        await asyncio.sleep(0.3)
+        await event.edit("ArRivEd")
+        await asyncio.sleep(0.3)
+        await event.edit("😎😎😎")
+        await asyncio.sleep(0.3)
+        await event.edit("EVERyBOdy wAs GanGeSTar UNtIL I ArRivEd 😎😎😎")		
 		
