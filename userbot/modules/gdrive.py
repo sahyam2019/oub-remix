c55793e358a364562c44fafa69f5933a98e2c088
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-22 17:39:48 +0530

    Update gdrive.py

--- userbot/modules/gdrive.py
+++ userbot/modules/gdrive.py
@@ -10,8 +10,8 @@ import time
 from pySmartDL import SmartDL
 from telethon import events
 from googleapiclient.discovery import build
-from apiclient.http import MediaFileUpload
-from apiclient.errors import ResumableUploadError
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.errors import ResumableUploadError
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.file import Storage
 from oauth2client import file, client, tools
