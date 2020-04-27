4027b30f0e9fd47a8c05987d903e3bcec5287c18
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-27 22:00:05 +0530

    Update uniborgmemes.py

--- userbot/modules/uniborgmemes.py
+++ userbot/modules/uniborgmemes.py
@@ -1210,7 +1210,7 @@ async def _(event):
 
             await event.edit(animation_chars[i % 27])	
 		
-@register(outgoing=True, pattern="^.think(?: |$)(.*)")
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
