ae65f1d104a44195e4ad46338f756f795a47cecc
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-27 21:47:54 +0530

    Update scrapers.py
    fixed  ud

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -251,37 +251,17 @@ async def wiki(wiki_q):
 
 
 @register(outgoing=True, pattern="^.ud (.*)")
-async def urban_dict(ud_e):
-    """ For .ud command, fetch content from Urban Dictionary. """
-    await ud_e.edit("Processing...")
-    query = ud_e.pattern_match.group(1)
-    try:
-        define(query)
-    except HTTPError:
-        await ud_e.edit(f"Sorry, couldn't find any results for: {query}")
+async def _(event):
+    if event.fwd_from:
         return
-    mean = define(query)
-    deflen = sum(len(i) for i in mean[0]["def"])
-    exalen = sum(len(i) for i in mean[0]["example"])
-    meanlen = deflen + exalen
-    if int(meanlen) >= 0:
-        if int(meanlen) >= 4096:
-            await ud_e.edit("`Output too large, sending as file.`")
-            file = open("output.txt", "w+")
-            file.write("Text: " + query + "\n\nMeaning: " + mean[0]["def"] +
-                       "\n\n" + "Example: \n" + mean[0]["example"])
-            file.close()
-            await ud_e.client.send_file(
-                ud_e.chat_id,
-                "output.txt",
-                caption="`Output was too large, sent it as a file.`")
-            if os.path.exists("output.txt"):
-                os.remove("output.txt")
-            await ud_e.delete()
-            return
-        await ud_e.edit("Text: **" + query + "**\n\nMeaning: **" +
-                        mean[0]["def"] + "**\n\n" + "Example: \n__" +
-                        mean[0]["example"] + "__")
+    await event.edit("processing...")
+    word = event.pattern_match.group(1)
+    urban = asyncurban.UrbanDictionary()
+    try:
+        mean = await urban.get_word(word)
+        await event.edit("Text: **{}**\n\nMeaning: **{}**\n\nExample: __{}__".format(mean.word, mean.definition, mean.example))
+    except asyncurban.WordNotFoundError:
+        await event.edit("No result found for **" + word + "**")
         if BOTLOG:
             await ud_e.client.send_message(
                 BOTLOG_CHATID,
