b7844226eb00642f47bf96ebbd49651fed814fd3
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-27 19:22:29 +0530

    Update uniborgmemes.py

--- userbot/modules/uniborgmemes.py
+++ userbot/modules/uniborgmemes.py
@@ -8,6 +8,7 @@ import asyncio
 from userbot.events import register 
 from userbot import  CMD_HELP, bot
 from collections import deque
+from telethon.errors.rpcerrorlist import MessageIdInvalidError
 
 
 
@@ -1010,6 +1011,29 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 19])
+		
+@register(outgoing=True, pattern="^.dumb(?: |$)(.*)")
+async def _(message):
+    try:
+        obj = message.pattern_match.group(1)
+        if len(obj) != 3:
+            raise IndexError
+        inp = ' '.join(obj)
+    except IndexError:
+        inp = "🥞 🎂 🍫"
+    u, t, g, o, s, n = inp.split(), '🗑', '<(^_^ <)', '(> ^_^)>', '⠀ ', '\n'
+    h = [(u[0], u[1], u[2]), (u[0], u[1], ''), (u[0], '', '')]
+    for something in reversed([y for y in ([''.join(x) for x in (
+    f + (s, g, s + s * f.count(''), t), f + (g, s * 2 + s * f.count(''), t),
+    f[:i] + (o, f[i], s * 2 + s * f.count(''), t), f[:i] + (s + s * f.count(''), o, f[i], s, t),
+    f[:i] + (s * 2 + s * f.count(''), o, f[i], t), f[:i] + (s * 3 + s * f.count(''), o, t),
+    f[:i] + (s * 3 + s * f.count(''), g, t))] for i, f in enumerate(reversed(h)))]):
+        for something_else in something:
+            await asyncio.sleep(0.3)
+            try:
+                await message.edit(something_else)
+            except MessageIdInvalidError:
+                return		
 
 CMD_HELP.update({
     "uniborgmemes":
@@ -1045,5 +1069,7 @@ CMD_HELP.update({
 \nUsage: For travelling from one place to another ✈️✈️\
 \n\n.jio\
 \nUsage: Your network slow?? Boost it using this 🤣🤣\
+\n\n.dumb\
+\nUsage: Throw waste out xD\	
 \n\n\nWARNING⚠️⚠️: All this cmds will spam group recents.\nUse it in OT groups/Spam groups OR GET YOU A** KICKED😂😂."
 })		
