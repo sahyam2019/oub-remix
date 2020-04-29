3c99a7deadec539d17fa1759eb54b2d69dcab806
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-29 19:58:59 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -5,7 +5,7 @@
 #
 """ Userbot module for getting information about the server. """
 
-from asyncio import create_subprocess_shell as asyncrunapp
+from asyncio import create_subprocess_exec as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
 from platform import python_version, uname
 from shutil import which
