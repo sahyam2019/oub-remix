ab82a7d13e69bad7780b6f924ed14565093b2357
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-29 21:22:33 +0530

    version 1.d

--- userbot/modules/github.py
+++ userbot/modules/github.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+
 import aiohttp
 from userbot.events import register
 from userbot import CMD_HELP
