8e1f66c6e2b17bb272809d673c38ff60dda9ed77
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-30 20:04:03 +0530

    Update uniborgmisc.py

--- userbot/modules/uniborgmisc.py
+++ userbot/modules/uniborgmisc.py
@@ -12,6 +12,21 @@ from urllib.parse import quote
 from datetime import datetime, timedelta
 from telethon.tl.types import UserStatusEmpty, UserStatusLastMonth, UserStatusLastWeek, UserStatusOffline, UserStatusOnline, UserStatusRecently, ChannelParticipantsKicked, ChatBannedRights
 from time import sleep
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+
+if 1 == 1:
+    name = "Profile Photos"
+    client = bot
+
 
 
 @register(outgoing=True, pattern="^.app(?: |$)(.*)")
@@ -312,4 +327,34 @@ async def ban_user(chat_id, i, rights):
     except Exception as exc:
         return False, str(exc)
        
-
+@register(outgoing=True, pattern="^.grab(?: |$)(.*)")
+    async def potocmd(event):
+        """Gets the profile photos of replied users, channels or chats"""
+        id = "".join(event.raw_text.split(maxsplit=2)[1:])
+        user = await event.get_reply_message()
+        chat = event.input_chat
+        if user:
+            photos = await event.client.get_profile_photos(user.sender)
+        else:
+            photos = await event.client.get_profile_photos(chat)
+        if id.strip() == "":
+            try:
+                await event.client.send_file(event.chat_id, photos)
+            except a:
+                photo = await event.client.download_profile_photo(chat)
+                await bot.send_file(event.chat_id, photo)
+        else:
+            try:
+                id = int(id)
+                if id <= 0:
+                    await event.edit("`ID number you entered is invalid`")
+                    return
+            except:
+                 await event.edit("`lol wtf`")
+                 return
+            if int(id) <= (len(photos)):
+                send_photos = await event.client.download_media(photos[id - 1])
+                await bot.send_file(event.chat_id, send_photos)
+            else:
+                await event.edit("`No photo found of that Nigga , now u Die`")
+                return
