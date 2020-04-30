57977e7061bfcb62087429c7ff5e24bca284e1c1
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-30 18:45:28 +0530

    Update uniborgmemes.py

--- userbot/modules/uniborgmemes.py
+++ userbot/modules/uniborgmemes.py
@@ -1781,7 +1781,7 @@ async def _(event):
 
         await event.edit("`\"If we put solar panels above parking lots, then our cars wouldn't get hot and we would have a lot of clean energy.\"`")
 
- @bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
 
