d521e2f70194d7e2886360f13a8e5b3035e6e3f7
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-30 16:23:58 +0530

    Update uniborgmemes.py

--- userbot/modules/uniborgmemes.py
+++ userbot/modules/uniborgmemes.py
@@ -1328,7 +1328,7 @@ async def _(event):
             await event.edit(animation_chars[i % 11])
 		
 		
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
 
