b4d9d5e963ea358bf141e79653c39505af508c09
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-30 21:06:27 +0530

    Update uniborgmisc.py

--- userbot/modules/uniborgmisc.py
+++ userbot/modules/uniborgmisc.py
@@ -23,7 +23,7 @@ from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo
 from telethon.tl.types import DocumentAttributeFilename
-from userbot.util import  humanbytes, progress, time_formatter
+from userbot.utils import  humanbytes, progress, time_formatter
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from telethon.errors import PhotoInvalidDimensionsError
