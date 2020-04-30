ccf4753e9398af96e170886a2d9a1c63e76e14d2
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-01 01:15:16 +0530

    Update remixmisc.py

--- userbot/modules/remixmisc.py
+++ userbot/modules/remixmisc.py
@@ -6,6 +6,7 @@ import os
 import asyncio
 import zipfile
 import time
+import html
 from io import BytesIO
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from telethon import *
@@ -498,6 +499,120 @@ async def _(event):
              await event.delete()
              await event.client.send_message(event.chat_id, response.message)
             
+            
+@register(outgoing=True, pattern="^.clone(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_message = await event.get_reply_message()
+    replied_user, error_i_a = await get_full_user(event)
+    if replied_user is None:
+        await event.edit(str(error_i_a))
+        return False
+    user_id = replied_user.user.id
+    profile_pic = await event.client.download_profile_photo(user_id, TEMP_DOWNLOAD_DIRECTORY)
+    # some people have weird HTML in their names
+    first_name = html.escape(replied_user.user.first_name)
+    # https://stackoverflow.com/a/5072031/4723940
+    # some Deleted Accounts do not have first_name
+    if first_name is not None:
+        # some weird people (like me) have more than 4096 characters in their names
+        first_name = first_name.replace("\u2060", "")
+    last_name = replied_user.user.last_name
+    # last_name is not Manadatory in @Telegram
+    if last_name is not None:
+        last_name = html.escape(last_name)
+        last_name = last_name.replace("\u2060", "")
+    if last_name is None:
+      last_name = "⁪⁬⁮⁮⁮⁮ ‌‌‌‌"
+    # inspired by https://telegram.dog/afsaI181
+    user_bio = replied_user.about
+    if user_bio is not None:
+        user_bio = html.escape(replied_user.about)
+    await bot(functions.account.UpdateProfileRequest(
+        first_name=first_name
+    ))
+    await bot(functions.account.UpdateProfileRequest(
+        last_name=last_name
+    ))
+    await bot(functions.account.UpdateProfileRequest(
+        about=user_bio
+    ))
+    n = 1
+    pfile = await bot.upload_file(profile_pic)  # pylint:disable=E060      
+    await bot(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
+        pfile
+    ))
+    #message_id_to_reply = event.message.reply_to_msg_id
+    #if not message_id_to_reply:
+    #    message_id_to_reply = event.message.id
+    #await borg.send_message(
+    #  event.chat_id,
+    #  "Hey ? Whats Up !",
+    #  reply_to=message_id_to_reply,
+    #  )
+    await event.delete()
+    await bot.send_message(
+      event.chat_id,
+      "**cloned like pero.**",
+      reply_to=reply_message
+      )
+
+async def get_full_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.forward:
+            replied_user = await event.client(
+                GetFullUserRequest(
+                    previous_message.forward.from_id or previous_message.forward.channel_id
+                )
+            )
+            return replied_user, None
+        else:
+            replied_user = await event.client(
+                GetFullUserRequest(
+                    previous_message.from_id
+                )
+            )
+            return replied_user, None
+    else:
+        input_str = None
+        try:
+            input_str = event.pattern_match.group(1)
+        except IndexError as e:
+            return None, e
+        if event.message.entities is not None:
+            mention_entity = event.message.entities
+            probable_user_mention_entity = mention_entity[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            else:
+                try:
+                    user_object = await event.client.get_entity(input_str)
+                    user_id = user_object.id
+                    replied_user = await event.client(GetFullUserRequest(user_id))
+                    return replied_user, None
+                except Exception as e:
+                    return None, e
+        elif event.is_private:
+            try:
+                user_id = event.chat_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            except Exception as e:
+                return None, e
+        else:
+            try:
+                user_object = await event.client.get_entity(int(input_str))
+                user_id = user_object.id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            except Exception as e:
+                return None, e
+            
+            
 CMD_HELP.update({
     "remixmisc":
     ".app\
@@ -514,6 +629,8 @@ CMD_HELP.update({
 \nUsage:replay .grab or .grab <count> to grab profile picture.\
 \n\n.watermark\
 \nusage: still fixing 😔\
+\n\n.clone @username\
+\nusage: clone you whole freking account except username so stay safe\
 \n\n.res\
 \nusage: type account,channel,group or bot username and reply with .res and check restriction\
 \n\n\n PS: I will add more xD" 
