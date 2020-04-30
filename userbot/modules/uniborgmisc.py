43e05082c80b25c5b95cc4e4a561bf11fc6ecaa7
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-30 18:18:35 +0530

    don't keng lemme fix

--- userbot/modules/uniborgmisc.py
+++ userbot/modules/uniborgmisc.py
@@ -6,6 +6,7 @@ from telethon import *
 from userbot.events import register 
 from userbot import CMD_HELP, bot
 from telethon import events
+from telethon.tl import functions, types
 
 @register(outgoing=True, pattern="^.app(?: |$)(.*)")
 async def apk(e):
@@ -78,6 +79,38 @@ async def _(event):
         await asyncio.sleep(3)
         await event.delete()
         
+@register(outgoing=True, pattern="^.calc(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input = event.pattern_match.group(1) #get input
+    exp = "Given expression is " + input #report back input
+    #lazy workaround to add support for two digits
+    final_input = tuple(input)
+    term1part1 = final_input[0]
+    term1part2 = final_input[1]
+    term1 = str(term1part1) + str(term1part2)
+    final_term1 = (int(term1))
+    operator = str(final_input[2])
+    term2part1 = final_input[3]
+    term2part2 = final_input[4]
+    term2 = str(term2part1) + str(term2part2)
+    final_term2 = (int(term2))
+    #actual calculations go here
+    if input == "help":
+        await event.edit("Syntax .calc <term1><operator><term2>\nFor eg .calc 02*02 or 99*99 (the zeros are important) (two terms and two digits max)")
+    elif operator == "*":
+        await event.edit("Solution -->\n" + exp + "\n" + str(final_term1 * final_term2))
+    elif operator == "-":
+        await event.edit("Solution -->\n" + exp + "\n" + str(final_term1 - final_term2))
+    elif operator == "+":
+        await event.edit("Solution -->\n" + exp + "\n" + str(final_term1 + final_term2))
+    elif operator == "/":
+        await event.edit("Solution -->\n" + exp + "\n" + str(final_term1 / final_term2))
+    elif operator == "%":
+        await event.edit("Solution -->\n" + exp + "\n" + str(final_term1 % final_term2))
+    else:
+        await event.edit("use .calc help")
         
         
         
