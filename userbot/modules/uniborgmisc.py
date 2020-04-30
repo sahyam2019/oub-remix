278d4e8527e398997e48f250d739ed7d1beee8c7
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-30 21:00:34 +0530

    Update uniborgmisc.py

--- userbot/modules/uniborgmisc.py
+++ userbot/modules/uniborgmisc.py
@@ -25,6 +25,10 @@ from telethon.tl.types import DocumentAttributeFilename
 from userbot.util import  humanbytes, progress, time_formatter
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
+from telethon.errors import PhotoInvalidDimensionsError
+
+from telethon.tl.functions.messages import SendMediaRequest
+
 
 import logging
 
@@ -488,3 +492,61 @@ def get_lst_of_files(input_directory, output_lst):
         output_lst.append(current_file_name)
     return output_lst
 
+@register(outgoing=True, pattern="^.z(?: |$)(.*)")
+async def on_file_to_photo(event):
+
+    await event.delete()
+
+    target = await event.get_reply_message()
+
+    try:
+
+        image = target.media.document
+
+    except AttributeError:
+
+        return
+
+    if not image.mime_type.startswith('image/'):
+
+        return  # This isn't an image
+
+    if image.mime_type == 'image/webp':
+
+        return  # Telegram doesn't let you directly send stickers as photos
+
+    if image.size > 10 * 1024 * 1024:
+
+        return  # We'd get PhotoSaveFileInvalidError otherwise
+
+
+
+    file = await bot.download_media(target, file=BytesIO())
+
+    file.seek(0)
+
+    img = await bot.upload_file(file)
+
+    img.name = 'image.png'
+
+
+
+    try:
+
+        await bot(SendMediaRequest(
+
+            peer=await event.get_input_chat(),
+
+            media=types.InputMediaUploadedPhoto(img),
+
+            message=target.message,
+
+            entities=target.entities,
+
+            reply_to_msg_id=target.id
+
+        ))
+
+    except PhotoInvalidDimensionsError:
+
+        return
