aae0ac17887e64e412f3cdf00dab04c0f55f302f
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-30 17:20:24 +0530

    hmm

---
+++ userbot/modules/getid_getbot.py
@@ -0,0 +1,61 @@
+#imported from uniborg to userbot by @heyworld
+
+from telethon import events
+from telethon.utils import pack_bot_file_id 
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantsBots
+from userbot.events import register 
+from userbot import CMD_HELP, bot
+
+@register(outgoing=True, pattern="^.getid(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.reply_to_msg_id:
+        chat = await event.get_input_chat()
+        r_msg = await event.get_reply_message()
+        if r_msg.media:
+            bot_api_file_id = pack_bot_file_id(r_msg.media)
+            await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(str(event.chat_id), str(r_msg.from_id), bot_api_file_id))
+        else:
+            await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`".format(str(event.chat_id), str(r_msg.from_id)))
+    else:
+        await event.edit("Current Chat ID: `{}`".format(str(event.chat_id)))
+        
+@register(outgoing=True, pattern="^.getbot(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "**Bots in this Channel**: \n"
+    input_str = event.pattern_match.group(1)
+    to_write_chat = await event.get_input_chat()
+    chat = None
+    if not input_str:
+        chat = to_write_chat
+    else:
+        mentions = "Bots in {} channel: \n".format(input_str)
+        try:
+            chat = await bot.get_entity(input_str)
+        except Exception as e:
+            await event.edit(str(e))
+            return None
+    try:
+        async for x in bot.iter_participants(chat, filter=ChannelParticipantsBots):
+            if isinstance(x.participant, ChannelParticipantAdmin):
+                mentions += "\n ⚜️ [{}](tg://user?id={}) `{}`".format(x.first_name, x.id, x.id)
+            else:
+                mentions += "\n [{}](tg://user?id={}) `{}`".format(x.first_name, x.id, x.id)
+    except Exception as e:
+        mentions += " " + str(e) + "\n"
+    await event.edit(mentions)
+    
+    
+ CMD_HELP.update({
+    "getid":
+    ".getid\
+\nUsage: Get ID of any Telegram media, or any user\
+\n\n.getbot\
+\nUsage: Get the Bots in any chat\
+"
+})
+       
+        
