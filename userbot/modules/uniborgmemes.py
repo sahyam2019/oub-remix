2a7a876a52d5318c7d1bf1b1262c36793027f45f
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-04-30 20:56:18 +0530

    Update uniborgmemes.py

--- userbot/modules/uniborgmemes.py
+++ userbot/modules/uniborgmemes.py
@@ -2333,7 +2333,448 @@ async def _(event):
 
         await event.edit("`\"What’s something you misunderstood as a child and only realized much later was wrong?\"`")		
    
-		
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.1
+
+    animation_ttl = range(0, 100)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "load":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "▮",
+
+            "▯",
+
+            "▬",
+
+            "▭"
+            "‎"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
+
+
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.1
+
+    animation_ttl = range(0, 100)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "square":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "◧",
+
+            "◨",
+
+            "◧",
+
+            "◨"
+            "‎"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
+
+
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.1
+
+    animation_ttl = range(0, 100)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "up":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "╹",
+
+            "╻",
+
+            "╹",
+
+            "╻"
+            "‎"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.1
+
+    animation_ttl = range(0, 100)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "round":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "⚫",
+
+            "⬤",
+
+            "●",
+
+            "∘"
+            "‎"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
+
+
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.1
+
+    animation_ttl = range(0, 100)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "heart":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "🖤",
+
+            "❤️",
+
+            "🖤",
+
+            "❤️"
+            "‎"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
+
+
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 1
+
+    animation_ttl = range(0, 11)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "anim":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "😁",
+
+            "😧",
+
+            "😡",
+
+            "😢",
+
+           
+ 
+            "😁",
+
+            "😧",
+
+            "😡",
+
+            "😢",
+
+           
+
+            "__**...BOY oh BOY! i feel like a wamen....**__"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 11])
+
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 2
+
+    animation_ttl = range(0, 6)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "fnl":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "😁🏿",
+
+            "😁🏾",
+
+            "😁🏽",
+
+            "😁🏼",
+
+            "‎😁",
+
+            "**Fair & Lovely GeNg Is BeHiNd You....**"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 6])
+
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 2
+
+    animation_ttl = range(0, 6)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "monkey":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "🐵",
+
+            "🙉",
+
+            "🙈",
+
+            "🙊",
+
+            "🖕‎🐵🖕",
+
+            "**OPPA MONEKEYY Style....**"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 6])
+
+            
+           
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 1
+
+    animation_ttl = range(0, 14)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "hand":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "👈",
+
+            "👉",
+
+            "☝️",
+
+            "👆",
+
+            "🖕",
+
+            "👇",
+
+            "✌️",
+
+            "🤞",
+
+            "🖖",
+
+            "🤘",
+
+            "🤙",
+
+            "🖐️",
+
+            "👌"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 14])
+
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 1
+
+    animation_ttl = range(0, 13)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "cnt":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "🔟",
+
+            "9️⃣",
+
+            "8️⃣",
+
+            "7️⃣",
+
+            "6️⃣",
+
+            "5️⃣",
+
+            "4️⃣",
+
+            "3️⃣",
+
+            "2️⃣",
+
+            "1️⃣",
+
+            "0️⃣",
+
+            "🆘"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 13])		
 		
 CMD_HELP.update({
     "uniborgmemes":
@@ -2354,6 +2795,8 @@ CMD_HELP.update({
 \n\n.qs\
 \nUsage: start conversation\
 \n\n.wtf\
+\nUsage: See it yourself\
+\n\n.load,.up,.square,.round,.heart,.monkey,.anim,.hand,.fnl,.cnt\
 \nUsage: See it yourself\
 \n\n.human\
 \nUsage: Nothing interesting\
