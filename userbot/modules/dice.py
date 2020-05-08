03c37bd42963700c1aaf1d19b23ba42192cd47c7
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-08 12:38:12 +0530

    Update and rename dice to dice.py

--- userbot/modules/dice.py
+++ userbot/modules/dice.py
@@ -1,8 +1,10 @@
+#fix by @heyworld for OUB
+#bug fixed by @d3athwarrior
 
 from telethon.tl.types import InputMediaDice
 #from uniborg.util import admin_cmd
 from userbot.events import register 
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot, ALIVE_NAME
+from userbot import CMD_HELP, bot
 
 
 
@@ -12,12 +14,19 @@ async def _(event):
         return
     input_str = event.pattern_match.group(1)
     await event.delete()
-    r = await event.reply(file=InputMediaDice())
+    r = await event.reply(file=InputMediaDice(''))
     if input_str:
         try:
             required_number = int(input_str)
             while not r.media.value == required_number:
                 await r.delete()
-                r = await event.reply(file=InputMediaDice())
+                r = await event.reply(file=InputMediaDice(''))
         except:
             pass
+
+        
+CMD_HELP.update({
+    "dice":
+    ".dice or .dice 1 to 6 any value\
+\nUsage: hahaha just a magic."
+})    
