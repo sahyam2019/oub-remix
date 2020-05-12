2522339930689344f7f059562f3c445ea1d11930
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-12 12:39:20 +0530

    testing

---
+++ userbot/modules/stickerdownlader.py
@@ -0,0 +1,43 @@
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from userbot import bot, CMD_HELP
+from userbot.events import register
+
+
+
+
+@register(outgoing=True, pattern="^.sd(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("```Reply to any user message.```")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.media:
+       await event.edit("```Reply to image or sticker```")
+       return
+    if not reply_message.text:
+       await event.edit("```Reply to a Link```")
+       return   
+    chat = "@Stickerdownloadbot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("```Reply to actual users message.```")
+       return
+    await event.edit("```processing```")
+    async with bot.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
+              await bot.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Please unblock @Stickerdownloadbot and try again```")
+              return
+          if response.text.startswith("I am really sorry!"):
+             await event.edit("```I think this is not the right link```")
+          else: 
+             await event.delete()   
+             await bot.forward_messages(event.chat_id, response.message)
