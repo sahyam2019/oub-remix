832ab40a66c4c5f0742c8d950373c7d10fb58cdb
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-12 10:56:45 +0530

    Update dice.py

--- userbot/modules/dice.py
+++ userbot/modules/dice.py
@@ -15,7 +15,27 @@ DICE = "🎲"
 # EMOJI CONSTANTS
 
 
-@register(outgoing=True, (pattern=f"^.{dice}|{dart})(?: |$)(.*)")
+
+@register(outgoing=True, (pattern=f"({DART}|{DICE}) ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_message = event
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+    emoticon = event.pattern_match.group(1)
+    input_str = event.pattern_match.group(2)
+    await event.delete()
+    r = await reply_message.reply(file=InputMediaDice(emoticon=emoticon))
+    if input_str:
+        try:
+            required_number = int(input_str)
+            while not r.media.value == required_number:
+                await r.delete()
+                r = await reply_message.reply(file=InputMediaDice(emoticon=emoticon))
+        except:
+            pass
+
 async def _(event):
     if event.fwd_from:
         return
