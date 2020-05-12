53f78fdcc6f5302a743a378af24025788fc596d5
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-12 10:21:32 +0530

    Update pm_logs.py

--- userbot/modules/pm_logs.py
+++ userbot/modules/pm_logs.py
@@ -1,16 +1,25 @@
-#this module originally created by @spechide https://github.com/SpEcHiDe/UniBorg
-#ported to oub by @heyworld
+#This module was created by @spechide for Uniborg
+"""Log PMs
+this will now log chat msgs to your botlog chat id.
+if you don't want chat logs than use `.nolog` .
+enjoy this now.
+Thanks to @heyworld for a small correction"""
 
 import asyncio
+import os
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
 from telethon import events
 from telethon.tl import functions, types
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot, TEMP_DOWNLOAD_DIRECTORY
 from userbot.events import register
-from userbot import BOTLOG, BOTLOG_CHATID, LOGS
 
 
 NO_PM_LOG_USERS = []
 
-
+#@borg.on(admin_cmd(incoming=True, func=lambda e: e.is_private))
 @register(incoming=True, disable_edited=True)
 async def monito_p_m_s(event):
     sender = await event.get_sender()
@@ -26,3 +35,18 @@ async def monito_p_m_s(event):
                 )
             except Exception as e:
                 LOGS.warn(str(e))
+
+#@borg.on(admin_cmd(pattern="nolog ?(.*)"))
+@register(pattern="^.nolog(?: |$)(.*)")
+async def approve_p_m(event):
+    if event.fwd_from:
+        return
+    reason = event.pattern_match.group(1)
+    chat = await event.get_chat()
+    if BOTLOG:
+        if event.is_private:
+            if chat.id not in NO_PM_LOG_USERS:
+                NO_PM_LOG_USERS.append(chat.id)
+                await event.edit("Won't Log Messages from this chat")
+                await asyncio.sleep(3)
+                await event.delete()
