3cc91af55bd090e066592d732793222d2ada7152
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-14 21:01:59 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -262,32 +262,42 @@ def errors_handler(func):
             pass
     return wrapper
 
-async def progress(current, total, event, start, type_of_ps, file_name=None):
-    """Generic progress_callback for both
-    upload.py and download.py"""
+async def progress(
+    current, total, gdrive, start, prog_type,
+    file_name=None, is_cancelled=False
+):
     now = time.time()
     diff = now - start
+    if is_cancelled is True:
+        raise CancelProcess
+
     if round(diff % 10.00) == 0 or current == total:
         percentage = current * 100 / total
         speed = current / diff
-        elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) * 1000
-        estimated_total_time = elapsed_time + time_to_completion
-        progress_str = "[{0}{1}]\nProgress: {2}%\n".format(
-            ''.join(["█" for i in range(math.floor(percentage / 5))]),
-            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
-            round(percentage, 2))
-        tmp = progress_str + \
-            "{0} of {1}\nETA: {2}".format(
-                humanbytes(current),
-                humanbytes(total),
-                time_formatter(estimated_total_time)
-            )
-        if file_name:
-            await event.edit("{}\nFile Name: `{}`\n{}".format(
-                type_of_ps, file_name, tmp))
+        elapsed_time = round(diff)
+        eta = round((total - current) / speed)
+        if 'upload' in prog_type.lower():
+            status = 'Uploading'
+        elif 'download' in prog_type.lower():
+            status = 'Downloading'
         else:
-            await event.edit("{}\n{}".format(type_of_ps, tmp))
+            status = 'Unknown'
+        progress_str = "`{0}` | [{1}{2}] `{3}%`".format(
+            status,
+            ''.join(["■" for i in range(
+                    math.floor(percentage / 10))]),
+            ''.join(["▨" for i in range(
+                    10 - math.floor(percentage / 10))]),
+            round(percentage, 2))
+        tmp = (
+            f"{progress_str}\n"
+            f"`{humanbytes(current)} of {humanbytes(total)}"
+            f" @ {humanbytes(speed)}`\n"
+            f"`ETA` -> {time_formatter(eta)}\n"
+            f"`Duration` -> {time_formatter(elapsed_time)}"
+        )
+        await gdrive.edit(f"`{prog_type}`\n\n"
+                          f"`Status`\n{tmp}")
             
             
 async def md5(fname: str) -> str:
