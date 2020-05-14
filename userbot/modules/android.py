373bb7657901c1f45ea8b5991c5b35c08e671014
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-14 19:22:26 +0530

    Update android.py

--- userbot/modules/android.py
+++ userbot/modules/android.py
@@ -45,7 +45,7 @@ async def magisk(request):
     await request.edit(releases)
     
     
- @register(outgoing=True, pattern="^.pixeldl(?: |$)(.*)")
+@register(outgoing=True, pattern="^.pixeldl(?: |$)(.*)")
 async def download_api(dl):
     await dl.edit("`Collecting information...`")
     URL = dl.pattern_match.group(1)
