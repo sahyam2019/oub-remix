e9941316e76896c3600e04d9e72e101712b4279b
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-15 14:16:00 +0530

    Update afk.py
    derpppp

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -20,30 +20,31 @@ from userbot.events import register
 
 # ========================= CONSTANTS ============================
 AFKSTR = [
-    "I'm busy right now. Please talk in a bag and when I come back you can just give me the bag!",
+    "`I'm busy right now. Please talk in a bag and when I come back you can just give me the bag!`",
     "I'm away right now. If you need anything, leave a message after the beep:\n`beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep`!",
-    "You missed me, next time aim better.",
-    "I'll be back in a few minutes and if I'm not...,\nwait longer.",
-    "I'm not here right now, so I'm probably somewhere else.",
-    "Roses are red,\nViolets are blue,\nLeave me a message,\nAnd I'll get back to you.",
-    "Sometimes the best things in life are worth waiting for…\nI'll be right back.",
-    "I'll be right back,\nbut if I'm not right back,\nI'll be back later.",
-    "If you haven't figured it out already,\nI'm not here.",
-    "Hello, welcome to my away message, how may I ignore you today?",
-    "I'm away over 7 seas and 7 countries,\n7 waters and 7 continents,\n7 mountains and 7 hills,\n7 plains and 7 mounds,\n7 pools and 7 lakes,\n7 springs and 7 meadows,\n7 cities and 7 neighborhoods,\n7 blocks and 7 houses...\n\nWhere not even your messages can reach me!",
-    "I'm away from the keyboard at the moment, but if you'll scream loud enough at your screen, I might just hear you.",
-    "I went that way\n---->",
-    "I went this way\n<----",
-    "Please leave a message and make me feel even more important than I already am.",
-    "I am not here so stop writing to me,\nor else you will find yourself with a screen full of your own messages.",
-    "If I were here,\nI'd tell you where I am.\n\nBut I'm not,\nso ask me when I return...",
-    "I am away!\nI don't know when I'll be back!\nHopefully a few minutes from now!",
-    "I'm not available right now so please leave your name, number, and address and I will stalk you later.",
-    "Sorry, I'm not here right now.\nFeel free to talk to my userbot as long as you like.\nI'll get back to you later.",
-    "I bet you were expecting an away message!",
-    "Life is so short, there are so many things to do...\nI'm away doing one of them..",
-    "I am not here right now...\nbut if I was...\n\nwouldn't that be awesome?",
+    "`You missed me, next time aim better.`",
+    "`I'll be back in a few minutes and if I'm not...,\nwait longer.`",
+    "`I'm not here right now, so I'm probably somewhere else.`",
+    "`Roses are red,\nViolets are blue,\nLeave me a message,\nAnd I'll get back to you.`",
+    "`Sometimes the best things in life are worth waiting for…\nI'll be right back.`",
+    "`I'll be right back,\nbut if I'm not right back,\nI'll be back later.`",
+    "`If you haven't figured it out already,\nI'm not here.`",
+    "`Hello, welcome to my away message, how may I ignore you today?`",
+    "`I'm away over 7 seas and 7 countries,\n7 waters and 7 continents,\n7 mountains and 7 hills,\n7 plains and 7 mounds,\n7 pools and 7 lakes,\n7 springs and 7 meadows,\n7 cities and 7 neighborhoods,\n7 blocks and 7 houses...\n\nWhere not even your messages can reach me!`",
+    "`I'm away from the keyboard at the moment, but if you'll scream loud enough at your screen, I might just hear you.`",
+    "`I went that way\n---->`",
+    "`I went this way\n<----`",
+    "`Please leave a message and make me feel even more important than I already am.`",
+    "`I am not here so stop writing to me,\nor else you will find yourself with a screen full of your own messages.`",
+    "`If I were here,\nI'd tell you where I am.\n\nBut I'm not,\nso ask me when I return...`",
+    "`I am away!\nI don't know when I'll be back!\nHopefully a few minutes from now!`",
+    "`I'm not available right now so please leave your name, number, and address and I will stalk you later.`",
+    "`Sorry, I'm not here right now.\nFeel free to talk to my userbot as long as you like.\nI'll get back to you later.`",
+    "`I bet you were expecting an away message!`",
+    "`Life is so short, there are so many things to do...\nI'm away doing one of them..`",
+    "`I am not here right now...\nbut if I was...\n\nwouldn't that be awesome?`",
 ]
+
 
 global USER_AFK  # pylint:disable=E0602
 global afk_time  # pylint:disable=E0602
@@ -74,10 +75,10 @@ async def set_afk(afk_e):
     afk_start = start_1.replace(microsecond=0)
     if string:
         AFKREASON = string
-        await afk_e.edit(f"Going AFK!\
+        await afk_e.edit(f"**Going AFK!**\
         \nReason: `{string}`")
     else:
-        await afk_e.edit("Going AFK!")
+        await afk_e.edit("**Going AFK!**")
     await afk_e.client(
         UpdateProfileRequest(first_name=user.first_name, last_name=user.last_name + " [ OFFLINE ]"))    
     if BOTLOG:
@@ -105,7 +106,7 @@ async def type_afk_is_not_true(notafk):
     afk_end = back_alive.replace(microsecond=0)
     if ISAFK:
         ISAFK = False
-        msg = await notafk.respond("I'm no longer AFK.")
+        msg = await notafk.respond("**I'm no longer AFK.**")
         time.sleep(3)
         await msg.delete()
         await notafk.client(UpdateProfileRequest(first_name=user.first_name, last_name=last1))
@@ -141,7 +142,7 @@ async def mention_afk(mention):
     user = await bot.get_me()
     back_alivee = datetime.now()
     afk_end = back_alivee.replace(microsecond=0)
-    afk_since = "a while ago"
+    afk_since = "**a while ago**"
     if mention.message.mentioned and not (await mention.get_sender()).bot:
         if ISAFK:
             now = datetime.now()
@@ -155,7 +156,7 @@ async def mention_afk(mention):
             time %= 60
             seconds = time
             if days == 1:
-                afk_since = "Yesterday"
+                afk_since = "**Yesterday**"
             elif days > 1:
                 if days > 6:
                     date = now + \
@@ -209,7 +210,7 @@ async def afk_on_pm(sender):
     user = await bot.get_me()
     back_alivee = datetime.now()
     afk_end = back_alivee.replace(microsecond=0)
-    afk_since = "a while ago"
+    afk_since = "**a while ago*"
     if sender.is_private and sender.sender_id != 777000 and not (
             await sender.get_sender()).bot:
         if PM_AUTO_BAN:
@@ -232,7 +233,7 @@ async def afk_on_pm(sender):
             time %= 60
             seconds = time
             if days == 1:
-                afk_since = "Yesterday"
+                afk_since = "**yesterday**"
             elif days > 1:
                 if days > 6:
                     date = now + \
