1dfa87cebe29a362da3209823b1db0f3fcaf66a7
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-15 18:15:05 +0530

    testing

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -12,15 +12,15 @@ from userbot.events import register
 
 @register(outgoing=True, pattern="^.covid (.*)")
 async def corona(event):
-    await event.edit("`Processing...`")
+    await event.edit("`searching...`")
     country = event.pattern_match.group(1)
     covid = Covid()
-    country_data = covid.get_status_by_country_name(country)
+    country_data = covid.get_country_name(country)
     if country_data:
-        output_text =  f"`Confirmed   : {country_data['confirmed']}`\n"
-        output_text += f"`Active      : {country_data['active']}`\n"
-        output_text += f"`Deaths      : {country_data['deaths']}`\n"
-        output_text += f"`Recovered   : {country_data['recovered']}`\n"
+        output_text =  f"`⚠️Confirmed   : {country_data['confirmed']}`\n"
+        output_text += f"`☢️Active      : {country_data['active']}`\n"
+        output_text += f"`⚰️Deaths      : {country_data['deaths']}`\n"
+        output_text += f"`♥️Recovered   : {country_data['recovered']}`\n"
         output_text += (
             "`Last update : "
             f"{datetime.utcfromtimestamp(country_data['last_update'] // 1000).strftime('%Y-%m-%d %H:%M:%S')}`\n"
@@ -30,9 +30,14 @@ async def corona(event):
         output_text = "No information yet about this country!"
     await event.edit(f"Corona Virus Info in {country}:\n\n{output_text}")
 
+def get_country_data(country, world):
+    for country_data in world:
+        if country_data["country"].lower() == country.lower():
+            return country_data
+    return {"Status": "No information yet about this country!"}
 
 CMD_HELP.update({
         "covid": 
-        ".covid <country>"
+        "`.covid` <country>"
         "\nUsage: Get an information about data covid-19 in your country.\n"
     })
