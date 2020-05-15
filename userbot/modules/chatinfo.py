d7fc6bc2622c6f1efc65cc0d3044850e8af3f95d
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-16 01:25:07 +0530

    Update chatinfo.py

--- userbot/modules/chatinfo.py
+++ userbot/modules/chatinfo.py
@@ -6,6 +6,7 @@
 # Credits to Hitalo-Sama and FTG Modules
 
 from datetime import datetime
+from telethon import functions
 from emoji import emojize
 from math import sqrt
 from telethon.tl.functions.channels import GetFullChannelRequest, GetParticipantsRequest
@@ -196,8 +197,42 @@ async def fetch_info(chat, event):
         caption += f"Description: \n<code>{description}</code>\n"
     return caption
 
+@register(outgoing=True, pattern="^.invite(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    to_add_users = event.pattern_match.group(1)
+    if event.is_private:
+        await event.edit("`.invite` users to a chat, not to a Private Message")
+    else:
+        if not event.is_channel and event.is_group:
+            # https://lonamiwebs.github.io/Telethon/methods/messages/add_chat_user.html
+            for user_id in to_add_users.split(" "):
+                try:
+                    await event.client(functions.messages.AddChatUserRequest(
+                        chat_id=event.chat_id,
+                        user_id=user_id,
+                        fwd_limit=1000000
+                    ))
+                except Exception as e:
+                    await event.reply(str(e))
+            await event.edit("Invited Successfully")
+        else:
+            # https://lonamiwebs.github.io/Telethon/methods/channels/invite_to_channel.html
+            for user_id in to_add_users.split(" "):
+                try:
+                    await event.client(functions.channels.InviteToChannelRequest(
+                        channel=event.chat_id,
+                        users=[user_id]
+                    ))
+                except Exception as e:
+                    await event.reply(str(e))
+            await event.edit("Invited Successfully")
+
 CMD_HELP.update({
         "chatinfo":
         ".chatinfo [optional: <reply/tag/chat id/invite link>]\
-            \nUsage: Gets info of a chat. Some info might be limited due to missing permissions."
+            \nUsage: Gets info of a chat. Some info might be limited due to missing permissions.\
+            \n\n.invite\
+            \nUsage:Invites user go a private chat,not in a private message."
 })
