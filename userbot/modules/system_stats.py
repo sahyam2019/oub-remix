7e9581f8af2534a67bcc5ff37babc59352a9d014
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-16 00:58:18 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -75,8 +75,7 @@ async def bot_ver(event):
             "Shame that you don't have git, You're running 9.0 - 'Extended' anyway"
         )
 
-
-@register(outgoing=True, pattern="^.pip(?: |$)(.*)")
+@register(outgoing=True, pattern="^\.pip(?: |$)(.*)")
 async def pipcheck(pip):
     """ For .pip command, do a pip search. """
     pipmodule = pip.pattern_match.group(1)
@@ -119,26 +118,6 @@ async def pipcheck(pip):
         await pip.edit("`Use .help pip to see an example`")
 
 
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot module for getting information about the server. """
-
-from asyncio import create_subprocess_exec as asyncrunapp
-from asyncio.subprocess import PIPE as asyncPIPE
-from platform import python_version, uname
-from shutil import which
-from os import remove
-from telethon import version
-
-from userbot import CMD_HELP, ALIVE_NAME
-from userbot.events import register
-
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-# ============================================
 
 
 @register(outgoing=True, pattern="^.sysd$")
