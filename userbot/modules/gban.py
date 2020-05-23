9772bffe304ce78776eb0741fd258b1869103028
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-23 22:54:17 +0530

    Update gban.py

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -31,7 +31,7 @@ async def _(event):
             G_BAN_LOGGER_GROUP,
             "/gban [user](tg://user?id={}) {}".format(r_from_id, reason)
         )
-    await event.delete()
+    await event.reply(f"user gbanned by @{DEFAULTUSER}")
 
 
 @register(outgoing=True, pattern="^.ungban(?: |$)(.*)")
@@ -49,7 +49,7 @@ async def _(event):
             G_BAN_LOGGER_GROUP,
             "/ungban [user](tg://user?id={}) {}".format(r_from_id, reason)
         )
-    await event.reply(f"user gbanned by @{DEFAULTUSER}")
+    await event.reply(f"user ungbanned by @{DEFAULTUSER}")
 CMD_HELP.update({
     "gban": "\
 `.gban reason`\
