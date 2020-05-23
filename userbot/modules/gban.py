00bab35b8cc14844deec0d8ee91f4c11fc8b30a0
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-23 21:40:01 +0530

    now u can gban from ub

---
+++ userbot/modules/gban.py
@@ -0,0 +1,56 @@
+"""Globally Ban users from all the
+Group Administrations bots where you are SUDO
+Available Commands:
+.gban REASON
+.ungban REASON"""
+from telethon import events
+import asyncio
+from userbot.events import register 
+from userbot import  CMD_HELP, bot, G_BAN_LOGGER_GROUP
+#imported from uniborg by @heyworld
+
+
+@register(outgoing=True, pattern="^.gban(?: |$)(.*)")
+async def _(event):
+    if G_BAN_LOGGER_GROUP is None:
+        await event.edit("ENV VAR is not set. This module will not work.")
+        return
+    if event.fwd_from:
+        return
+    reason = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        r = await event.get_reply_message()
+        if r.forward:
+            r_from_id = r.forward.from_id or r.from_id
+        else:
+            r_from_id = r.from_id
+        await bot.send_message(
+            G_BAN_LOGGER_GROUP,
+            "!gban [user](tg://user?id={}) {}".format(r_from_id, reason)
+        )
+    await event.delete()
+
+
+@register(outgoing=True, pattern="^.ungban(?: |$)(.*)")
+async def _(event):
+    if G_BAN_LOGGER_GROUP is None:
+        await event.edit("ENV VAR is not set. This module will not work.")
+        return
+    if event.fwd_from:
+        return
+    reason = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        r = await event.get_reply_message()
+        r_from_id = r.from_id
+        await bot.send_message(
+            G_BAN_LOGGER_GROUP,
+            "!ungban [user](tg://user?id={}) {}".format(r_from_id, reason)
+        )
+    await event.delete()
+CMD_HELP.update({
+    "gban": "\
+`.gban reason`\
+\nUsage: Globally Ban users from all the Group Administrations bots where you are SUDO.\
+\n\n`.ungban reason`\
+\nUsage: Globally unBan users from all the Group Administrations bots where you are SUDO"
+})
