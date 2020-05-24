cf3a308acf6e058694749abc787365ba1d07ef2b
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-24 13:10:20 +0530

    Update warns_sql.py

--- userbot/modules/sql_helper/warns_sql.py
+++ userbot/modules/sql_helper/warns_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Integer, Column, String, UnicodeText, func, distinct, Boolean
 
-from sql_helper import SESSION, BASE
+from userbot.modules.sql_helper import SESSION, BASE
 
 
 class Warns(BASE):
