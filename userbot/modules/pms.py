80718f9885e695ed244d8fd5511f381eff3456da
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-27 20:04:51 +0530

    life without derps?

--- userbot/modules/pms.py
+++ userbot/modules/pms.py
@@ -439,9 +439,9 @@ CMD_HELP.update({
 \nUsage: Clears/Disables any notifications of unapproved PMs.\
 \n\n`.notifon`\
 \nUsage: Allows notifications for unapproved PMs.\
-\n\n`.pmute\
+\n\n`.pmute`\
 \nUsage: Reply .pmute and it will mute that person in pm<can be used in group also>.\
-\n\n``punmute``\
+\n\n`punmute`\
 \nUsage: Reply .punmute and it will unmute that person in pm.\
 \n\n`logpms`\
 \nUsage: If you don't want chat logs than use `.nolog` , for opposite use `.log`. Default is .log enabled\nThis will now log chat msgs to your PM_LOGGR_BOT_API_ID.\
