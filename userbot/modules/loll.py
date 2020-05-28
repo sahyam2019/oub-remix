c5159b4914c66e3c3a15183abe5157fe8855a5a9
S A Sohan <50193747+shn999@users.noreply.github.com>
2020-05-29 01:06:43 +0600

    Uploaded New Plugin

---
+++ userbot/modules/loll.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You can find misc modules, which dont fit in anything xD
+""" Userbot module for other small commands. """
+
+from random import randint
+from asyncio import sleep
+from os import execl
+import sys
+import os
+import io
+import sys
+import json
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
+from userbot.events import register
+
+@register(outgoing=True, pattern="^.loll$")
+async def loll(e):
+    await e.edit("😂\n😂\n😂\n😂\n😂😂😂😂\n\n   😂😂😂\n 😂         😂\n😂           😂\n 😂         😂\n   😂😂😂\n\n😂\n😂\n😂\n😂\n😂😂😂😂")
+
+        
+    
+    CMD_HELP.update({
+    'loll':
+    '.loll\
+\nUsage: gives a nice LOL as output.'
+})
+
