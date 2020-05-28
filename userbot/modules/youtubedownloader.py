bd2641fb28345eb2f855a64fdb057754beb20ab7
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-05-29 02:31:34 +0530

    Test 5

--- userbot/modules/youtubedownloader.py
+++ userbot/modules/youtubedownloader.py
@@ -22,7 +22,7 @@ thumb_image_path = TEMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
 if not os.path.isdir(out_folder):
     os.makedirs(out_folder)
 
-@register(outgoing=True, pattern="^.ytdl(a|v)$")
+@register(outgoing=True, pattern="ut(a|v) (.*)"))
 async def download_video(v_url):
     """ For .ytdl command, download media from YouTube and many other sites. """
     url = v_url.pattern_match.group(2)
@@ -158,7 +158,7 @@ async def download_video(v_url):
         os.remove(f"{ytdl_data['id']}.mp4")
         await v_url.delete()
 
-@register(outgoing=True, pattern="^.playlist(a|v)$")
+@register(outgoing=True, pattern="playlist(a|v) (.*)"))
 async def download_video(v_url):
     """ For .ytdl command, download media from YouTube and many other sites. """
     url = v_url.pattern_match.group(2)
