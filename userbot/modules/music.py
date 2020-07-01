bb859bc408d190a379ac69e31c6f88d24e4543e3
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-07-01 14:44:44 +0530

    life without derps?

--- userbot/modules/music.py
+++ userbot/modules/music.py
@@ -31,31 +31,6 @@ def bruh(name):
     os.system("instantmusic -q -s "+name)
     
 
-@register(outgoing=True, pattern="^.song(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    DELAY_BETWEEN_EDITS = 0.3
-    PROCESS_RUN_TIME = 100
-    cmd = event.pattern_match.group(1)
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    await event.edit("searching song..please wait")    
-    bruh(str(cmd))
-    l = glob.glob("*.mp3")
-    loa = l[0]
-    await event.edit("sending song")
-    await bot.send_file(
-                event.chat_id,
-                loa,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id
-            )
-    os.system("rm -rf *.mp3")
-    subprocess.check_output("rm -rf *.mp3",shell=True)
 
 @register(outgoing=True, pattern="^.spd(?: |$)(.*)")
 async def _(event):
@@ -135,9 +110,7 @@ async def DeezLoader(Deezlod):
     
 CMD_HELP.update({
         "music":
-        "`.song` <search title>\
-            \nUsage: For searching songs.\
-            \n\n`.spd`<Artist - Song Title>\
+        ".spd`<Artist - Song Title>\
             \nUsage:For searching songs from Spotify.\
             \n\n`.netease` <Artist - Song Title>\
             \nUsage:Download music with @WooMaiBot\
