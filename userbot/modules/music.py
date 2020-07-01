f0f5ce67b456e024dc35324ec39bbcfdb3470aeb
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-07-01 15:01:20 +0530

    kek

--- userbot/modules/music.py
+++ userbot/modules/music.py
@@ -118,8 +118,9 @@ CMD_HELP.update({
             \nUsage:Download music from Spotify or Deezer.\
             \n\n`.deezload` <spotify/deezer link> <Format>\
             \nUsage: Download music from deezer.\
-            \n\n Well deezer is not available in India so create an deezer account using vpn. Set DEEZER_ARL_TOKEN in vars to make this work.\nVideo guide of arl token: [here](https://www.youtube.com/watch?v=O6PRT47_yds&feature=youtu.be) or read [this](https://notabug.org/RemixDevs/DeezloaderRemix/wiki/Login+via+userToken).\
-            \n\n *Format= `FLAC`, `MP3_320`, `MP3_256`, `MP3_128`."
+            \n\n Well deezer is not available in India so create an deezer account using vpn. Set DEEZER_ARL_TOKEN in vars to make this work.\
+            \n\n *Format= `FLAC`, `MP3_320`, `MP3_256`, `MP3_128`.\
+            \n\n\n Guide:Video guide of arl token: [here](https://www.youtube.com/watch?v=O6PRT47_yds&feature=youtu.be) or Read [This](https://notabug.org/RemixDevs/DeezloaderRemix/wiki/Login+via+userToken)."
 })
 
 
