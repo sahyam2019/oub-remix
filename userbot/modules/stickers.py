fee8309dfbce1ecd74c36cf0977b40652b3d369b
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-07-31 15:35:37 +0530

    auto-delete kang msg

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -168,6 +168,8 @@ async def kang(event):
         f"{emoji} as the emoji! "
         f"This pack can be found `[here](t.me/addstickers/{packname})",
         parse_mode='md')
+    await asyncio.sleep(7.5)
+    await args.delete()    
 
 
 async def newpack(is_anim, sticker, emoji, packtitle, packname):
