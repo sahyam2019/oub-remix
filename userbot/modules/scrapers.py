a40b9d9ed89192ee4d5f9828c15604f8d91d953d
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-08-17 17:20:07 +0530

    userbot: New imdb module

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -1295,7 +1295,90 @@ async def capture(url):
                                    caption=input_str,
                                    force_document=True,
                                    reply_to=message_id)
-        await url.delete()        
+        await url.delete()
+
+@register(outgoing=True, pattern="^.imdb (.*)")
+async def imdb(e):
+    try:
+        movie_name = e.pattern_match.group(1)
+        remove_space = movie_name.split(' ')
+        final_name = '+'.join(remove_space)
+        page = get("https://www.imdb.com/find?ref_=nv_sr_fn&q=" + final_name +
+                   "&s=all")
+        lnk = str(page.status_code)
+        soup = BeautifulSoup(page.content, 'lxml')
+        odds = soup.findAll("tr", "odd")
+        mov_title = odds[0].findNext('td').findNext('td').text
+        mov_link = "http://www.imdb.com/" + \
+            odds[0].findNext('td').findNext('td').a['href']
+        page1 = get(mov_link)
+        soup = BeautifulSoup(page1.content, 'lxml')
+        if soup.find('div', 'poster'):
+            poster = soup.find('div', 'poster').img['src']
+        else:
+            poster = ''
+        if soup.find('div', 'title_wrapper'):
+            pg = soup.find('div', 'title_wrapper').findNext('div').text
+            mov_details = re.sub(r'\s+', ' ', pg)
+        else:
+            mov_details = ''
+        credits = soup.findAll('div', 'credit_summary_item')
+        if len(credits) == 1:
+            director = credits[0].a.text
+            writer = 'Not available'
+            stars = 'Not available'
+        elif len(credits) > 2:
+            director = credits[0].a.text
+            writer = credits[1].a.text
+            actors = []
+            for x in credits[2].findAll('a'):
+                actors.append(x.text)
+            actors.pop()
+            stars = actors[0] + ',' + actors[1] + ',' + actors[2]
+        else:
+            director = credits[0].a.text
+            writer = 'Not available'
+            actors = []
+            for x in credits[1].findAll('a'):
+                actors.append(x.text)
+            actors.pop()
+            stars = actors[0] + ',' + actors[1] + ',' + actors[2]
+        if soup.find('div', "inline canwrap"):
+            story_line = soup.find('div',
+                                   "inline canwrap").findAll('p')[0].text
+        else:
+            story_line = 'Not available'
+        info = soup.findAll('div', "txt-block")
+        if info:
+            mov_country = []
+            mov_language = []
+            for node in info:
+                a = node.findAll('a')
+                for i in a:
+                    if "country_of_origin" in i['href']:
+                        mov_country.append(i.text)
+                    elif "primary_language" in i['href']:
+                        mov_language.append(i.text)
+        if soup.findAll('div', "ratingValue"):
+            for r in soup.findAll('div', "ratingValue"):
+                mov_rating = r.strong['title']
+        else:
+            mov_rating = 'Not available'
+        await e.edit('<a href=' + poster + '>&#8203;</a>'
+                     '<b>Title : </b><code>' + mov_title + '</code>\n<code>' +
+                     mov_details + '</code>\n<b>Rating : </b><code>' +
+                     mov_rating + '</code>\n<b>Country : </b><code>' +
+                     mov_country[0] + '</code>\n<b>Language : </b><code>' +
+                     mov_language[0] + '</code>\n<b>Director : </b><code>' +
+                     director + '</code>\n<b>Writer : </b><code>' + writer +
+                     '</code>\n<b>Stars : </b><code>' + stars +
+                     '</code>\n<b>IMDB Url : </b>' + mov_link +
+                     '\n<b>Story Line : </b>' + story_line,
+                     link_preview=True,
+                     parse_mode='HTML')
+    except IndexError:
+        await e.edit("Plox enter **Valid movie name** kthx")
+
 
 CMD_HELP.update({
     "scrappers":
@@ -1338,5 +1421,7 @@ CMD_HELP.update({
 \n\nSupported Urls: `Google Drive` - `Cloud Mail` - `Yandex.Disk` - `AFH` - `ZippyShare` - `MediaFire` - `SourceForge` - `OSDN` - `GitHub`\
 \n\n`.ss <url>`\
 \nUsage: Takes a screenshot of a website and sends the screenshot.\
-\nExample of a valid URL : `https://www.google.com`"    
+\nExample of a valid URL : `https://www.google.com`\
+\n\n`.imdb` movie/series name\
+\nUsage:scrap movie/series information."
 })
