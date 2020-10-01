e72ceaa3e221d424cd0e49001b4583d818cb5f46
Maxpayne7000 <61088084+Maxpayne7000@users.noreply.github.com>
2020-10-01 16:07:08 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -226,11 +226,11 @@ async def amireallyalive(alive):
              f"`Telethon version`: {version.__version__} \n"
              f"`Python version`: {python_version()} \n"
              f"`Bot Version: Remix {BOT_VER}` \n"
-             f"==================================== \n"
+ 
              f"`User`: {DEFAULTUSER} \n"
 
-             f"`Bot Uptime ⏱️`: {uptime} \n"
-             f"====================================\n")
+             f"`Bot Uptime`: {uptime} \n"
+
     if ALIVE_LOGO:
         try:
             logo = ALIVE_LOGO
