2a8abbec1a1798b937188d0ccac0cc98746e7561
Maxpayne7000 <61088084+Maxpayne7000@users.noreply.github.com>
2020-10-01 12:01:17 +0530

    Update fakegban.py

--- userbot/modules/fakegban.py
+++ userbot/modules/fakegban.py
@@ -20,7 +20,7 @@ async def gbun(event):
     gbunVar = gbunVar[6:]
     mentions = f"`Warning!! User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By` {DEFAULTUSER}\n"
     no_reason = "No Reason Given "
-    await event.edit("**Summoning out the mighty gban hammer ☠️**")
+    await event.edit("Summoning out the mighty gban hammer")
     asyncio.sleep(3.5)
     chat = await event.get_input_chat()
     async for x in bot.iter_participants(chat, filter=ChannelParticipantsAdmins):
