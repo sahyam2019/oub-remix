91b049da1712e561877c19b6cbf1162743293d14
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-11-01 22:42:55 +0530

    userbot: Added 🎰 emoji

--- userbot/modules/emojigames.py
+++ userbot/modules/emojigames.py
@@ -72,9 +72,26 @@ async def _(event):
                 await r.delete()
                 r = await event.reply(file=InputMediaDice('⚽'))
         except:
-            pass        
+            pass
+     
+@register(outgoing=True, pattern="^.jackpot(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.delete()
+    r = await event.reply(file=InputMediaDice('🎰'))
+    if input_str:
+        try:
+            required_number = int(input_str)
+            while not r.media.value == required_number:
+                await r.delete()
+                r = await event.reply(file=InputMediaDice('🎰'))
+        except:
+            pass 
+
 CMD_HELP.update({
     "emojigames":
-    "`.dice` 1-6 or `.dart`1-6 or `.bb`1-5 or `.fb`1-5\
+    "`.dice` 1-6 or `.dart`1-6 or `.bb`1-5 or `.fb`1-5 or '.jackpot 1-5'\
 \nUsage: hahaha just a magic.\nWarning:`Don't use any other values or bot will crash`"
 })    
