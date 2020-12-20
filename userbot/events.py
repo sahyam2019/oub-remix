c7cc103089e240be17de98844e10dcd865faceec
ZettaAlpha <50816765+ZettaAlpha@users.noreply.github.com>
2020-12-20 07:37:27 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -24,7 +24,7 @@ def register(**args):
     pattern = args.get('pattern', None)
     disable_edited = args.get('disable_edited', True)
     ignore_unsafe = args.get('ignore_unsafe', False)
-    unsafe_pattern = r'^[^/!#@\$A-Za-z]'
+    unsafe_pattern = r'^[^/!#@:,;'?\$A-Za-z]'
     groups_only = args.get('groups_only', False)
     trigger_on_fwd = args.get('trigger_on_fwd', False)
     disable_errors = args.get('disable_errors', False)
